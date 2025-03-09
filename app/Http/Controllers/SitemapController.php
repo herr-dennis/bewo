993776198ceb234219f54ed591407367565a2e26
herr-dennis <herr.dennisblacky@hotmail.de>
2025-03-09 21:22:08 +0100

    9.3.25 fix

---
+++ app/Http/Controllers/SitemapController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Sitemap\SitemapGenerator;
+
+class SitemapController extends Controller
+{
+    public function generateSitemap()
+    {
+        $sitemap = SitemapGenerator::create(config('app.url'))
+            ->getSitemap();
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
+
+        return response()->json(['message' => 'Sitemap generated successfully!']);
+    }
+}
